4fa64f41716e7dfdc6541c65715b38dd351fa7a6
tielan <505725798@qq.com>
2022-09-10 20:27:16 +0800

    a

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 3000;
 let ioMapper = {};
 
+app.get("/app", (req, res) => {
+  res.location("http://" + ioMapper.ip + ":9902");
+});
+
 app.get("/gip", (req, res) => {
   res.send(ioMapper.ip || "");
 });
@@ -10,8 +14,8 @@ app.get("/gip", (req, res) => {
 app.get("/ip", (req, res) => {
   let ip = getClientIp(req).match(/\d+.\d+.\d+.\d+/);
   ip = ip ? ip.join(".") : null;
-  ioMapper = {ip}
-  console.log(ip)
+  ioMapper = { ip };
+  console.log(ip);
   res.send("ok");
 });
 
@@ -19,13 +23,12 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-
 function getClientIp(req) {
-    return (
-      req.headers["x-forwarded-for"] ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      req.connection.socket.remoteAddress ||
-      ""
-    );
-  };
+  return (
+    req.headers["x-forwarded-for"] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress ||
+    ""
+  );
+}
