df085b7277bfd205ed94c7942ccd60391edb72e3
tielan <505725798@qq.com>
2022-09-10 18:39:08 +0800

    a

--- index.js
+++ index.js
@@ -4,14 +4,27 @@ const port = 3000;
 let ioMapper = {};
 
 app.get("/gip", (req, res) => {
-  res.send(ioMapper.host || '');
+  res.send(ioMapper.host || "");
 });
 
 app.get("/ip", (req, res) => {
-  ioMapper = req.query;
-  res.send("Hello World!");
+  let ip = getClientIp(req).match(/\d+.\d+.\d+.\d+/);
+  ip = ip ? ip.join(".") : null;
+  ioMapper = {ip}
+  res.send("ok");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+function getClientIp(req) {
+    return (
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress ||
+      ""
+    );
+  };
